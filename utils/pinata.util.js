f42f3e957973719f5d1603bc314b6514127bed55
sai shravan <saishravan384@gmail.com>
2024-10-04 15:03:41 +0530

    feat: Use Pinata's image optimizations

--- utils/pinata.util.js
+++ utils/pinata.util.js
@@ -3,6 +3,9 @@ const pinata = require('../config/pinata.config')
 const { Blob } = require('buffer')
 const fs = require('fs')
 
+/**
+ * Uploads a given image to Pinata and returns the uploaded url. The image is optimized
+ */
 exports.getUploadedUrl = asyncHandler(async (fileToUpload) => {
   const blob = new Blob([fs.readFileSync(fileToUpload.path)])
 
@@ -14,10 +17,20 @@ exports.getUploadedUrl = asyncHandler(async (fileToUpload) => {
 
   fs.unlink(fileToUpload.path, (err) => err && console.log(err))
 
-  const url = await pinata.gateways.createSignedURL({
-    cid: upload.cid,
-    expires: 5000000,
-  })
+  const url = await pinata.gateways
+    .createSignedURL({
+      cid: upload.cid,
+      expires: 5000000,
+    })
+    .optimizeImage({
+      width: 500,
+      height: 500,
+      format: 'webp',
+      fit: 'cover',
+      gravity: 'auto',
+      format: 'webp',
+      sharpen: 5,
+    })
 
   return url
 })
