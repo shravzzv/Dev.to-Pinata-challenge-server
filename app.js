a845de0ef43b9ffabda8d936fb92f655a5e79c48
sai shravan <saishravan384@gmail.com>
2024-10-08 15:46:26 +0530

    feat: Add cors middleware

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+const cors = require('cors')
 require('dotenv').config()
 
 require('./config/db.config')
@@ -11,6 +12,7 @@ const pinsRouter = require('./routes/pins')
 
 const app = express()
 
+app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
